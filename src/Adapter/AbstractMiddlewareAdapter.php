5d0ac0b25b910d779e2967bbc96fc1798a276cab
Boris Verhaaff <boris@websoftwar.es>
2015-05-17 10:49:55 +0200

    Added Abstract adapter and first adapter acquia/http-hmac-php

---
+++ src/Adapter/AbstractMiddlewareAdapter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Websoftwares\Middleware\Adapter;
+
+use Websoftwares\Middleware\MiddlewareInterface;
+
+/**
+ * class AbstractMiddlewareAdapter.
+ *
+ * @author Boris <boris@websoftwar.es>
+ */
+abstract class AbstractMiddlewareAdapter implements MiddlewareInterface
+{
+}
---
+++ tests/Adapter/RequestAuthenticatorAdapterTest.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Websoftwares\Test\Middleware\Adapter;
+
+use Websoftwares\Middleware\Adapter\RequestAuthenticatorAdapter;
+use Phly\Http\ServerRequestFactory;
+use Acquia\Hmac\RequestAuthenticator;
+use Acquia\Hmac\RequestSigner;
+use Acquia\Hmac\Test\DummyKeyLoader;
+
+/**
+ * Class RequestAuthenticatorAdapterTest.
+ */
+class RequestAuthenticatorAdapterTest extends \PHPUnit_Framework_TestCase
+{
+    protected $response;
+
+    public function setUp()
+    {
+        $this->response = $this->getMock('Psr\Http\Message\ResponseInterface');
+    }
+
+    public function testValidSignature()
+    {
+        $signer = new RequestSigner();
+        $signer->addCustomHeader('Custom1');
+
+        $headers = array(
+            'Content-Type' => 'text/plain',
+            'Date' => 'Fri, 19 Mar 1982 00:00:04 GMT',
+            'Authorization' => 'Acquia 1:'.'WkDE6I+sDSqSKKYnZrHhT3MiHLU=',
+            'Custom1' => 'Value1',
+        );
+
+        $request = $this->newRequest();
+
+        // Add headers
+        foreach ($headers as $header => $value) {
+            $request = $request->withHeader($header, $value);
+        }
+
+        $authenticator = new RequestAuthenticator($signer, 0);
+        $keyLoader = new DummyKeyLoader();
+
+        $authenticator = new RequestAuthenticatorAdapter($authenticator, $keyLoader);
+
+        $this->assertNull($authenticator($request, $this->response));
+    }
+
+    /**
+     * @expectedException \Acquia\Hmac\Exception\InvalidSignatureException
+     */
+    public function testInvalidSignature()
+    {
+        $signer = new RequestSigner();
+        $signer->addCustomHeader('Custom1');
+
+        $headers = array(
+            'Content-Type' => 'text/plain',
+            'Date' => 'Fri, 19 Mar 1982 00:00:04 GMT',
+            'Authorization' => 'Acquia 1:badsignature',
+            'Custom1' => 'Value1',
+        );
+
+        $request = $this->newRequest();
+
+        // Add headers
+        foreach ($headers as $header => $value) {
+            $request = $request->withHeader($header, $value);
+        }
+
+        $authenticator = new RequestAuthenticator($signer, 0);
+        $keyLoader = new DummyKeyLoader();
+
+        $authenticator = new RequestAuthenticatorAdapter($authenticator, $keyLoader);
+        $authenticator($request, $this->response);
+    }
+
+    /**
+     * @expectedException \Acquia\Hmac\Exception\TimestampOutOfRangeException
+     */
+    public function testExpiredRequest()
+    {
+        $signer = new RequestSigner();
+        $headers = array(
+            'Content-Type' => 'text/plain',
+            'Date' => 'Fri, 19 Mar 1982 00:00:04 GMT',
+            'Authorization' => 'Acquia 1:'.'WkDE6I+sDSqSKKYnZrHhT3MiHLU=',
+        );
+
+        $request = $this->newRequest();
+
+        // Add headers
+        foreach ($headers as $header => $value) {
+            $request = $request->withHeader($header, $value);
+        }
+
+        $authenticator = new RequestAuthenticator($signer, '10 minutes');
+        $keyLoader = new DummyKeyLoader();
+
+        $authenticator = new RequestAuthenticatorAdapter($authenticator, $keyLoader);
+
+        $authenticator($request, $this->response);
+    }
+
+    /**
+     * @expectedException \Acquia\Hmac\Exception\TimestampOutOfRangeException
+     */
+    public function testFutureRequest()
+    {
+        $signer = new RequestSigner();
+        $time = new \DateTime('+11 minutes');
+
+        $headers = array(
+            'Content-Type' => 'text/plain',
+            'Date' => $time->format('D, d M Y H:i:s \G\M\T'),
+            'Authorization' => 'Acquia 1:'.'WkDE6I+sDSqSKKYnZrHhT3MiHLU=',
+        );
+
+        $request = $this->newRequest();
+
+        // Add headers
+        foreach ($headers as $header => $value) {
+            $request = $request->withHeader($header, $value);
+        }
+
+        $authenticator = new RequestAuthenticator($signer, '10 minutes');
+        $keyLoader = new DummyKeyLoader();
+
+        $authenticator = new RequestAuthenticatorAdapter($authenticator, $keyLoader);
+
+        $authenticator($request, $this->response);
+    }
+
+    protected function newRequest()
+    {
+        $server = [
+            'REQUEST_METHOD' => 'POST',
+            'REQUEST_URI' => '/resource/1?key=value',
+            'QUERY_STRING' => 'key=value',
+        ];
+
+        $server = array_merge($_SERVER, $server);
+
+        return ServerRequestFactory::fromGlobals($server);
+    }
+}
